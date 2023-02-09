2bfc6f0cfd1c02a3c5ea52533f851483472d5303
ulises-mendez <elarcano.sistemas@gmail.com>
2023-02-09 12:26:50 -0600

    NextJS with lottie animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,54 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import lottie from 'lottie-web';
+import React, { useEffect, useRef } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+    const ref = useRef<HTMLElement | null>()
+
+    useEffect(() => {
+        if (ref.current) {
+          return
+        }
+        ref.current = document.getElementById('lottie')
+    
+        window.lottie.useWebWorker(true)
+        window.lottie.setQuality('low')
+    
+        const anim = window.lottie.loadAnimation({
+          container: ref.current,
+          renderer: 'svg',
+          loop: true,
+          autoplay: true,
+          rendererSettings: {
+            progressiveLoad: true,
+            preserveAspectRatio: 'xMidYMid meet',
+            imagePreserveAspectRatio: 'xMidYMid meet',
+            title: 'TEST TITLE',
+            description: 'TEST DESCRIPTION',
+          },
+          path: '../animation.json',
+        })
+        anim.setSubframe(false)
+        anim.onError = function (errorType: any) {
+          console.log(errorType)
+        }
+    
+        anim.addEventListener('error', function (error: any) {
+          console.log(error)
+        })
+    
+        anim.addEventListener('error', function (error: any) {
+          console.log(error)
+        })
+    
+        anim.addEventListener('DOMLoaded', function () {
+          console.log('DOMLoaded')
+        })
+      }, [])
   return (
     <>
       <Head>
@@ -40,6 +84,8 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
+        <div id='lottie' style={{ width: 200, margin: 'auto', display: 'block' }}></div>
+
           <Image
             className={styles.logo}
             src="/next.svg"
